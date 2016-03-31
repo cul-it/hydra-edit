ca877c3d36dc6b65fa141a28b6cb8299d9a7d1e7
John A. Cline <jac244@cornell.edu>
2016-03-31 09:28:09 -0400

    change head to heading in page.rb. add create/update scripts for pages.

--- app/models/page.rb
+++ app/models/page.rb
@@ -3,11 +3,23 @@
 class Page < ActiveFedora::Base
   include ::CurationConcerns::WorkBehavior
   include ::BasicMetadata
+  include ::RequiredMetadata
   validates :title, presence: { message: 'Your work must have a title.' }
   
   property :our_identifier, predicate: ::RDF::URI("http://purl.org/dc/terms/identifier") do |index|
        index.as :stored_searchable
   end
-  
+  property :heading, predicate: ::RDF::URI("http://opaquenamespace.org/terms/heading") do |index|
+       index.as :stored_searchable
+  end
+  property :node, predicate: ::RDF::URI("http://opaquenamespace.org/terms/node") do |index|
+       index.as :stored_searchable
+  end
+  property :node_type, predicate: ::RDF::URI("http://opaquenamespace.org/terms/node_type") do |index|
+       index.as :stored_searchable
+  end
+  property :page_number, predicate: ::RDF::URI("http://opaquenamespace.org/terms/page_number") do |index|
+       index.as :stored_searchable
+  end  
   
 end
